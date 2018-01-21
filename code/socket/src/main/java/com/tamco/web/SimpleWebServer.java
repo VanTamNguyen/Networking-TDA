62624cb86509644b74cbed6494242f80cd891849
VanTamNguyen <vantamnguyen.vnu@gmail.com>
2018-01-21 20:17:26 +0700

    Simple Web Server

--- code/socket/src/main/java/com/tamco/web/SimpleWebServer.java
+++ code/socket/src/main/java/com/tamco/web/SimpleWebServer.java
@@ -1,18 +1,21 @@
 package com.tamco.web;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SimpleWebServer {
 
-    public static final int PORT = 8080;
+    public static final int PORT = 54000;
 
     private ServerSocket serverSocket;
 
     private ExecutorService requestHandlingPool;
+
+    private SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:Ss z");
 
     public SimpleWebServer() { }
 
@@ -36,16 +39,49 @@ public class SimpleWebServer {
     private void handleRequest(Socket clientSocket) {
         Runnable handlingTask = () -> {
             try {
-                clientSocket.getInputStream();
-                clientSocket.getOutputStream();
-            } catch (IOException e) {
+                InputStream inputStream = clientSocket.getInputStream();
+                OutputStream outputStream = clientSocket.getOutputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
+                String line; StringBuilder builder = new StringBuilder("<html><body><h1>Welcome to TamCO Web Server</h1><p>");
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                    builder.append(line).append("<br/>");
+                    if (line.isEmpty()) break;
+                }
+                builder.append("</p></body></html>");
+
+                String body = builder.toString();
+                String res = "HTTP/1.1 200 OK\n"
+                + "Server: TamCO/0.1\r\n"
+                + "Date: " + format.format(new java.util.Date()) + "\n"
+                + "Content-Type: text/html; charset=UTF-8\n"
+                + "Connection: close\n"
+                + "Content-Length: " + body.getBytes().length + "\n\n"
+                + body;
+
+                outputStream.write(res.getBytes());
+                outputStream.flush();
+                outputStream.close();
+                inputStream.close();
+                bufferedReader.close();
+
+                clientSocket.close();
+
+            } catch (IOException e) {
+                System.out.println("Error while handling HTTP Request: " + e.getMessage());
             }
         };
         requestHandlingPool.submit(handlingTask);
     }
 
     public static void main(String[] args) {
-
+        SimpleWebServer webServer = new SimpleWebServer();
+        try {
+            webServer.init();
+            webServer.startServing();
+        } catch (IOException e) {
+            System.out.println("Error occurred while starting web server: " + e.getMessage());
+        }
     }
 }
